31a119d6d42975c9d84a87455a2e278e7b225297
Naharashu <130159705+Naharashu@users.noreply.github.com>
2025-03-23 21:50:38 +0200

    Add files via upload

--- src/quantum.py
+++ src/quantum.py
@@ -1,14 +1,31 @@
 import fpp
+import os
 
 aaaaan = 0
 while True:
     while aaaaan < 1:
-        print("Welcome to Quantum. Its ide for F++. Current version is 0.1.0")
-        print("Type 'exit' to exit")
+        print("Welcome to Quantum. Its ide for F++. Current version is 0.2.0")
+        print("Type 'exit' to exit and 'help' to see commands")
         aaaaan += 1
     text = input("Quantum > ")
     if text == "exit":
-        break
+        confirm = input("Are you sure you want to exit? (yes/no): ")
+        if confirm.lower() == "yes" or confirm.lower() == "y":
+            break
+        else:
+            continue
+    elif text == "release notes":
+        print("Current version is 0.2.0 (Release-23032025)")
+        print("- Added 'release notes' command")
+        print("- Added 'help' command")
+        print("- Upgraded 'exit' command")
+        continue
+    elif text == "help":
+        print("Type 'exit' to exit")
+        print("Type 'release notes' to see release notes")
+        print("Type 'help' to see help")
+        continue
+
     result, error = fpp.run('<stdin>' ,text)
 
     if error: print(error.as_string())
